61fd74ed48131c48671153203de96b5c767c5d9e
Daniel Kurth <daniel.kurth@sap.com>
2022-09-28 09:16:25 +0200

    initial commit

---
+++ src/main/java/com/moonstonks/moonstonksbackendnew/MoonstonksBackendNewApplication.java
@@ -0,0 +1,13 @@
+package com.moonstonks.moonstonksbackendnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonstonksBackendNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonstonksBackendNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moonstonks/moonstonksbackendnew/MoonstonksBackendNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonstonks.moonstonksbackendnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonstonksBackendNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
